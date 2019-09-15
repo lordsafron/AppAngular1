fbfdabe6b902a2db3893ed266e6a8209e3bcb3a9
Hugo Martinez <hmmartinez88@gmail.com>
2019-09-15 10:08:08 -0500

    Angular spa1

---
+++ src/app/components/resultado-heroe/resultado-heroe.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-resultado-heroe',
+  templateUrl: './resultado-heroe.component.html'
+})
+export class ResultadoHeroeComponent implements OnInit {
+
+  @Input() heroe: any = {};
+  @Input() indice: number;
+
+  heroes: Heroe[] = [];
+  termino: string;
+
+  constructor( private heroesService: HeroesService,
+               private activatedRoute: ActivatedRoute ) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this.heroesService.buscarHeroes( params['termino'] );
+      console.log( this.heroes );
+    });
+  }
+
+}
